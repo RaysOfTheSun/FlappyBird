2b3d182741bb855dfb88cecc8d4459f5962e7884
Carl <rdelsol35@gmail.com>
2018-07-05 20:18:20 +0800

    update Bird class

--- Javascript/GameModules/Game.js
+++ Javascript/GameModules/Game.js
@@ -3,9 +3,14 @@ let bird;
 function setup() {
     createCanvas(720, 400);
     bird = new Bird(0, height/2)
+    frameRate(60);
 }
 
 function draw() { 
     background(0);   // Set the background to black
     bird.toCanvas()
-  } 
+}
+
+function keyPressed() {
+    bird.jump();
+}
--- Javascript/GameObjects/Bird.js
+++ Javascript/GameObjects/Bird.js
@@ -12,15 +12,73 @@ class Bird {
         this.x_coordinate = x_coordinate;
         this.y_coordinate = y_coordinate;
 
-        this.birdWidth = 40;
+        this.birdWidth = 40; // Determines the height and width of the bird
+        this.pull = -14; // application will result into negative velocity
+        this.gravity = 1; // the force the pulls the bird downward
+        this.velocity = -2; // dictates the speed and direction of the bird
 
-        this.upperLimit = height / this.birdWidth
-        this.lowerLimit = (height - this.birdWidth) - this.ground_offset
-        this.sprites = new Sprite("res/Images/bird_wing_down.png")
+        // The bird should now go over the upper and lower bounds of the canvas
+        this.upperLimit = height / this.birdWidth;
+        this.lowerLimit = (height - this.birdWidth) - this.ground_offset;
+
+        this.sprites = new Sprite("res/Images/bird_wing_down.png");
+        this.jumped = false;
     }
 
+    // Draws the bird onto the specified canvas or surface
+    // [canvas]: The surface wherein the bird is to be drawn on
     toCanvas() {
-        this.sprites.toCanvas(this.x_coordinate, this.y_coordinate)
+
+        if (this.jumped) {
+            this.fly();
+        }
+        else {
+            this.fall();
+        }
+
+        this.updatePosition();
+
+        this.sprites.toCanvas(this.x_coordinate, this.y_coordinate, 
+            this.birdWidth, this.birdWidth);
+    }
+
+    fall() {
+        if (this.y_coordinate != this.lowerLimit) {
+            this.velocity += this.gravity;
+            this.pull = -14;
+        }
+    }
+
+    fly() {
+        if (this.y_coordinate > this.upperLimit) { 
+            this.pull += 2;
+            this.velocity += this.pull;
+        }
+
+        this.jumped = false;
+    }
+
+    updatePosition() {
+        if ((this.y_coordinate <= 1) && (!this.jumped)) {
+            this.y_coordinate = this.upperLimit;
+            this.velocity = 0;
+        }
+        else if ((this.y_coordinate >= (height - this.ground_offset))) {
+            this.y_coordinate = this.lowerLimit;
+            this.velocity = 0;
+        }
+        else {
+            this.y_coordinate += this.velocity;
+        }
+    }
+
+    jump() {
+        this.jumped = true;
+    }
+
+    reset() {
+        this.x_coordinate = 60;
+        this.y_coordinate = height / 2;
     }
 
 
